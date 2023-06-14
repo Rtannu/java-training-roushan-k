70073d46fbc5bc92ea9d117bcab97ae40f869870
Rtannu <info.raj.mnit@gmail.com>
2023-06-14 12:43:17 -0500

    Called Extending and Runnable Interface

---
+++ src/rtn/day12/assignment12/Main.java
@@ -0,0 +1,17 @@
+package rtn.day12.assignment12;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println(" ********************************************** Create a Thread by Extending Thread Class  *********************************************");
+        ExtendingThread extendingThread=new ExtendingThread();
+        extendingThread.start();
+
+        System.out.println(" ********************************************** Create a Thread by Implementing Runnable Interface  *********************************************");
+        ImplementingRunnableInterface implementingRunnableInterface=new ImplementingRunnableInterface();
+        Thread thread=new Thread(implementingRunnableInterface);
+        thread.start();
+
+    }
+}
