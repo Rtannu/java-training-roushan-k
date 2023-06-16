bacbe8c4231c75bc4580d153e089043035d05665
Rtannu <info.raj.mnit@gmail.com>
2023-06-16 13:41:42 -0500

    completed assignment-13

---
+++ src/rtn/day13/assignment13/DeadLockSecondThread.java
@@ -0,0 +1,25 @@
+package rtn.day13.assignment13;
+
+public class DeadLockSecondThread implements Runnable{
+
+    final static String firstResource="FIRST_RESOURCE";
+    final static String secondResource="SECOND_RESOURCE";
+    @Override
+    public void run() {
+
+        synchronized (secondResource){
+            System.out.println("Thread :: "+Thread.currentThread().getId()+" Locked the "+secondResource);
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+
+            }
+            synchronized (firstResource){
+                System.out.println("Thread :: "+Thread.currentThread().getId()+" Locked the "+firstResource);
+            }
+
+        }
+
+
+    }
+}
---
+++ src/rtn/day13/assignment13/TicketBookingSystem.java
@@ -0,0 +1,20 @@
+package rtn.day13.assignment13;
+
+public class TicketBookingSystem {
+    int totalTickets;
+    int bookedTickets;
+
+    TicketBookingSystem(int totalTickets){
+        this.totalTickets=totalTickets;
+        this.bookedTickets=0;
+    }
+
+    public synchronized void book(){
+        if(totalTickets<=0){
+            throw new RuntimeException("Tickets are not available");
+        }
+        totalTickets-=1;
+        bookedTickets+=1;
+        System.out.println("Thread :: "+Thread.currentThread().getId() +" Booked Ticket :: "+bookedTickets +" and Total Available Tickets :: "+totalTickets);
+    }
+}
